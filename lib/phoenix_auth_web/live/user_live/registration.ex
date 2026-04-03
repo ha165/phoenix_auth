db6b88472affe5a7797c48ef9967f7efdc55b7b9
ha165 <lumumbaharmony@gmail.com>
2026-04-03 16:23:19 +0300

    change margin

--- lib/phoenix_auth_web/live/user_live/registration.ex
+++ lib/phoenix_auth_web/live/user_live/registration.ex
@@ -8,7 +8,7 @@ defmodule PhoenixAuthWeb.UserLive.Registration do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash} current_scope={@current_scope}>
-      <div class="mx-auto max-w-sm">
+      <div class="mx-auto max-w-sm -mt-34">
         <div class="text-center">
           <.header>
             Register for an account
