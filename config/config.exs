a68e317fc4c692eced6abc768bb1bcc969a82d59
ha165 <lumumbaharmony@gmail.com>
2026-04-03 12:04:21 +0300

    authentication scaffolding

--- config/config.exs
+++ config/config.exs
@@ -7,6 +7,19 @@
 # General application configuration
 import Config
 
+config :phoenix_auth, :scopes,
+  user: [
+    default: true,
+    module: PhoenixAuth.Accounts.Scope,
+    assign_key: :current_scope,
+    access_path: [:user, :id],
+    schema_key: :user_id,
+    schema_type: :id,
+    schema_table: :users,
+    test_data_fixture: PhoenixAuth.AccountsFixtures,
+    test_setup_helper: :register_and_log_in_user
+  ]
+
 config :phoenix_auth,
   ecto_repos: [PhoenixAuth.Repo],
   generators: [timestamp_type: :utc_datetime]
