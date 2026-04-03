58e40b687f0a5594f9c05004756d1b4f29f7872d
ha165 <lumumbaharmony@gmail.com>
2026-04-03 15:10:24 +0300

    alter usertable

--- priv/repo/migrations/20260403120007_add_user_fields.exs
+++ priv/repo/migrations/20260403120007_add_user_fields.exs
@@ -2,6 +2,10 @@ defmodule PhoenixAuth.Repo.Migrations.AddUserFields do
   use Ecto.Migration
 
   def change do
-
+    alter table(:users) do
+      add :first_name, :string
+      add :last_name, :string
+      add :DOB, :date
+    end
   end
 end
