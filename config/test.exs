0c8e98927bb5d2c6567389265f005b0b9d1560dc
ha165 <lumumbaharmony@gmail.com>
2026-04-03 11:55:59 +0300

    update tests

--- config/test.exs
+++ config/test.exs
@@ -7,7 +7,7 @@ import Config
 # Run `mix help test` for more information.
 config :phoenix_auth, PhoenixAuth.Repo,
   username: "postgres",
-  password: "postgres",
+  password: "4869",
   hostname: "localhost",
   database: "phoenix_auth_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
